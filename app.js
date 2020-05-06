09af84df99770db3ad4175b061acc78f25beb36b
Muhamad Rizqi Farhan <rizqifarhan20@gmail.com>
2020-05-06 19:02:23 +0700

    Adding yargs and its dependencies

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-const request = require('request')
-const fs = require('fs')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
+const yargs = require('yargs')
 
+// App Versioning
+yargs.version('1.0.0')
 
-geocode ('Sukabumi', (error, data) => {
-  if (error) {
-    return console.log(error)
-  }
-
-  forecast (data.latitude + ',' + data.longitude, (error, forecastData) => {
-    if (error) {
-      return console.log(error)
+yargs.command({
+  command: 'weather',
+  describe: 'Searching forecasting weather data by place name',
+  builder: {
+    place: {
+      describe: 'Place name',
+      demandOption: true,
+      type: 'string'
     }
+  },
+  handler(argv) {
+    geocode (argv.place, (error, data) => {
+      if (error) {
+        return console.log(error)
+      }
 
-    console.log(data.place)
-    console.log(forecastData)
-  })
+      forecast (data.latitude + ',' + data.longitude, (error, forecastData) => {
+        if (error) {
+          return console.log(error)
+        }
+
+        console.log(data.place)
+        console.log(forecastData)
+      })
+    })
+  }
 })
+
+yargs.parse()
